257024b768c1911a6a413045ed6ef66693d797d6
pasha7308 <pasha7308@mail.ru>
2018-06-16 15:01:32 +0700

    cleanup

--- src/main/java/com/pasha/service/Downloader.java
+++ src/main/java/com/pasha/service/Downloader.java
@@ -10,8 +10,6 @@ import java.net.URL;
 public class Downloader<T> {
     // https://survarium.pro/api/v2/players/pasha7308/stats?lang=english&limit=15&skip=0
     static private final String URL = "https://survarium.pro/api/v2/players/%s/stats?lang=english&limit=%d&skip=%d";
-    static private final String URLPASHA = "https://survarium.pro/api/v2/players/pasha7308/stats";
-    static private final String URLALKARIZ = "https://survarium.pro/api/v2/players/Alkariz/stats";
     static private final String AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/17.17134";
 
     private final Class<T> typeParameterClass;
